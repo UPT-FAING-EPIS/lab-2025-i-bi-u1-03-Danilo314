746a22c9014d6aa08302f263330fa7293fd9e0a4
Brian Chite <bc2021070015@virtual.upt.pe>
2025-04-19 11:19:18 -0500

    Avance 05

--- infra/inframain.tf
+++ infra/inframain.tf
@@ -1,33 +1,27 @@
+# infra/main.tf
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-sql-demo"
+  name     = "rg-database-demo"
   location = "East US"
 }
 
-resource "azurerm_sql_server" "sql_server" {
-  name                         = "sqlserverdemo123"
+resource "azurerm_sql_server" "sqlserver" {
+  name                         = "sqlserverdemotf"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "sqladminuser"
-  administrator_login_password = "YourP@ssw0rd123" # Usa variables seguras en producción
+  administrator_login_password = "P@ssword1234"
 }
 
-resource "azurerm_sql_database" "sql_db" {
-  name                = "demo-db"
+resource "azurerm_sql_database" "sqldb" {
+  name                = "sqldbdemo"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sql_server.name
-  sku_name            = "Basic"
-}
-
-output "sql_server_name" {
-  value = azurerm_sql_server.sql_server.name
-}
-
-output "sql_db_name" {
-  value = azurerm_sql_database.sql_db.name
+  server_name         = azurerm_sql_server.sqlserver.name
+  sku_name            = "S0"
 }
