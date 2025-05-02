5f0b48bb36ffbcbd620261c9929f300c779b2df5
Danilo314 <bc2021070015@virtual.upt.pe>
2025-05-02 12:20:37 -0500

    Loading2 Lab03

--- infra/main.tf
+++ infra/main.tf
@@ -1,28 +1,47 @@
 provider "azurerm" {
-  features {}
-  subscription_id = "f7782463-8cd3-492c-b5fa-57ef6878a5fa"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-sql-demo"
+resource "azurerm_resource_group" "danilo_rg" {
+  name     = "danilo-rg"
   location = "West US 2"
 }
 
-resource "azurerm_mssql_server" "sqlserver" {
-  name                         = "sqlserverdemotf"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
+resource "azurerm_mssql_server" "danilo_sql_server" {
+  name                         = "danilo-sqlserver"
+  resource_group_name          = azurerm_resource_group.danilo_rg.name
+  location                     = azurerm_resource_group.danilo_rg.location
   version                      = "12.0"
-  administrator_login          = "sqladminuser"
-  administrator_login_password = "P@ssword1234!" # ⚠️ Asegúrate de que cumpla las políticas de seguridad
+  administrator_login          = var.sqladmin_username
+  administrator_login_password = var.sqladmin_password
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-resource "azurerm_mssql_database" "sqldb" {
-  name           = "mi-sql-db"
-  server_id      = azurerm_mssql_server.sqlserver.id
-  sku_name       = "Basic"
+resource "azurerm_mssql_database" "danilo_database" {
+  name                         = "danilo-db"
+  server_id                    = azurerm_mssql_server.danilo_sql_server.id
+  sku_name                     = "GP_S_Gen5_2"
+  collation                    = "SQL_Latin1_General_CP1_CI_AS"
+  auto_pause_delay_in_minutes = 60
+  min_capacity                 = 0.5
+  storage_account_type         = "Local"
+}
 
-  tags = {
-    environment = "dev"
-  }
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "danilo-fw-allow-azure"
+  server_id        = azurerm_mssql_server.danilo_sql_server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
 }
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,5 +1,6 @@
-resource_group_name = "chite-rg-sql-lab01"
-sql_server_name     = "sqlserverlab01brianchite"
-admin_username      = "sqladminuser"
-admin_password      = "Danilo123456@"  # cambia esta por seguridad
-database_name       = "lab01db-chite"
+subscription_id   = "f7782463-8cd3-492c-b5fa-57ef6878a5fa"
+client_id         = "ae60ceb2-98ae-4ee9-b94e-4f9621eea492"
+client_secret = var.azurerm_client_secret
+tenant_id         = "b6b466ee-468d-4011-b9fc-fbdcf82ac90a"
+sqladmin_username = "danilo"
+sqladmin_password = "Danilo123456@"
