90df0760c6bbc6e2c5b726b8c6b82d42249d2a1d
Danilo314 <bc2021070015@virtual.upt.pe>
2025-05-02 05:17:24 -0500

    Remove2 LAB03

--- infra/main.tf
+++ infra/main.tf
@@ -1,25 +1,28 @@
 provider "azurerm" {
   features {}
+  subscription_id = "f7782463-8cd3-492c-b5fa-57ef6878a5fa"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-db"
-  location = "East US"
+  name     = "rg-sql-demo"
+  location = "West US 2"
 }
 
-resource "azurerm_sql_server" "sqlserver" {
-  name                         = "sqlserverdemo"
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "sqlserverdemotf"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "adminuser"
-  administrator_login_password = "StrongP@ss123"
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "P@ssword1234!" # ⚠️ Asegúrate de que cumpla las políticas de seguridad
 }
 
-resource "azurerm_sql_database" "sqldb" {
-  name                = "mydb"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sqlserver.name
-  sku_name            = "S0"
+resource "azurerm_mssql_database" "sqldb" {
+  name           = "mi-sql-db"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  sku_name       = "Basic"
+
+  tags = {
+    environment = "dev"
+  }
 }
---
+++ infra/terraform.tfvars
@@ -0,0 +1,5 @@
+resource_group_name = "chite-rg-sql-lab01"
+sql_server_name     = "sqlserverlab01brianchite"
+admin_username      = "sqladminuser"
+admin_password      = "Danilo123456@"  # cambia esta por seguridad
+database_name       = "lab01db-chite"
